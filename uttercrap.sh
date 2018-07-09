e0914aaf9c9f02b73bf7c5e27700361a1558a989
dchebito <39344240+dchebito@users.noreply.github.com>
2018-07-09 17:42:07 +0300

    Update uttercrap.sh
    Extend syntax, fix multiple separators issue.

--- uttercrap.sh
+++ uttercrap.sh
@@ -1,36 +1,42 @@
-awk '			\
+awk -F '[ ]' '			\
 BEGIN			\
 {			\
 	srand();	\
+	FPAT = "([[:space:]]*[[:alnum:][:punct:][:digit:]]+)"; \
 }			\
 			\
 {			\
 	for (i = 1; i <= NF; ++i)				\
 	{							\
-		r = int(rand() * 16);				\
-		if (r == 5)					\
-			sub(",", ", gosh, ", $i);		\
-		else if (r == 6)				\
-			sub(",", ", damn, ", $i);		\
-		else if (r == 7)				\
-			sub(",", ", crap, ", $i);		\
+		r = int(rand() * 22);				\
+		if (r == 7)					\
+			sub(",", ", literally,", $i);		\
 		else if (r == 8)				\
-			sub(",", ", basically, ", $i);		\
+			sub(",", ", kinda", $i);		\
 		else if (r == 9)				\
-			sub(",", ", right,", $i);		\
+			sub(",", ", sorta", $i);		\
 		else if (r == 10)				\
-			sub(",", ", you know,", $i);		\
+			sub(",", ", well,", $i);		\
 		else if (r == 11)				\
-			sub(",", ", I mean,", $i);		\
+			sub(",", ", right,", $i);		\
 		else if (r == 12)				\
-			sub(",", ", uhm,", $i);			\
+			sub(",", ", you know,", $i);		\
 		else if (r == 13)				\
-			sub(",", ", well,", $i);			\
+			sub(",", ", uhm,", $i);			\
 		else if (r == 14)				\
 			sub(",", ", okay,", $i);		\
 		else if (r == 15)				\
-			sub(",", ", actually,", $i)		\
+			sub(",", ", like", $i);			\
+		else if (r == 16)				\
+			sub(",", ", I mean,", $i);		\
+		else if (r == 17)				\
+			sub(",", ", you see,", $i);		\
+		else if (r == 18)				\
+			sub(",", ", believe me,", $i);		\
+		else if (r == 19)				\
+			sub(",", ", I guess,", $i);		\
+		else if (r == 20)				\
+			sub(",", ", or something", $i)		\
 	}							\
 	print $0						\
 }'
-
